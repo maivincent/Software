05a80bceca56d7e2f6dd36328238f551ea486f25
Dzenan Lapandic <ldzenan@student.ethz.ch>
2018-05-23 17:51:39 +0200

    fix

--- catkin_ws/src/20-indefinite-navigation/fleet_planning/src/actions_dispatcher_node.py
+++ catkin_ws/src/20-indefinite-navigation/fleet_planning/src/actions_dispatcher_node.py
@@ -11,7 +11,7 @@ import tf2_ros
 from fleet_planning.generate_duckietown_map import graph_creator
 from fleet_planning.location_to_graph_mapping import IntersectionMapper
 from fleet_planning.message_serialization import InstructionMessageSerializer, LocalizationMessageSerializer
-from duckietown_msgs.msg import BoolStamped, FSMState, Twist2DStamped
+from duckietown_msgs.msg import BoolStamped, FSMState, Twist2DStamped, MaintenanceState
 from fleet_planning.duckiebot import TaxiEvent, NO_TARGET_LOCATION
 # from rgb_led.srv import PlayLEDPattern
 
