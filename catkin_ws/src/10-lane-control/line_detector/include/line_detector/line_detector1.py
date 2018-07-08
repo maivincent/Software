9b8f20806fe242df68f971a24eefd3d7e6024757
Vincent Mai <vincent.mai@umontreal.ca>
2018-07-08 16:04:24 -0500

    Take out norms computation line detector

--- catkin_ws/src/10-lane-control/line_detector/include/line_detector/line_detector1.py
+++ catkin_ws/src/10-lane-control/line_detector/include/line_detector/line_detector1.py
@@ -35,6 +35,21 @@ class LineDetectorHSV(Configurable, LineDetectorInterface):
         configuration = copy.deepcopy(configuration)
         Configurable.__init__(self, param_names, configuration)
 
+        self.dilation_kernel_size = 3
+        self.canny_thresholds = [80,200]
+        self.hough_threshold = 2
+        self.hough_min_line_length = 3
+        self.hough_max_line_gap = 1
+     
+        self.hsv_white1 = [0,0,150]
+        self.hsv_white2 = [180,60,255]
+        self.hsv_yellow1 = [165,140,100]
+        self.hsv_yellow2 = [180,255,255]
+        self.hsv_red1 = [0,140,100]
+        self.hsv_red2 = [15,255,255]
+        self.hsv_red3 = [25,140,100]
+        self.hsv_red4 = [45,255,255]
+
     def _colorFilter(self, color):
         # threshold colors in HSV space
         if color == 'white':
@@ -68,64 +83,17 @@ class LineDetectorHSV(Configurable, LineDetectorInterface):
         else:
             lines = []
         return lines
-    
-    def _checkBounds(self, val, bound):
-        val[val<0]=0
-        val[val>=bound]=bound-1
-        return val
-
-    def _correctPixelOrdering(self, lines, normals):
-        flag = ((lines[:,2]-lines[:,0])*normals[:,1] - (lines[:,3]-lines[:,1])*normals[:,0])>0
-        for i in range(len(lines)):
-            if flag[i]:
-                x1,y1,x2,y2 = lines[i, :]
-                lines[i, :] = [x2,y2,x1,y1] 
  
     def _findNormal(self, bw, lines):
-        normals = []
-        centers = []
-        if len(lines)>0:
-            length = np.sum((lines[:, 0:2] -lines[:, 2:4])**2, axis=1, keepdims=True)**0.5
-            dx = 1.* (lines[:,3:4]-lines[:,1:2])/length
-            dy = 1.* (lines[:,0:1]-lines[:,2:3])/length
-
-            centers = np.hstack([(lines[:,0:1]+lines[:,2:3])/2, (lines[:,1:2]+lines[:,3:4])/2])
-            x3 = (centers[:,0:1] - 3.*dx).astype('int')
-            y3 = (centers[:,1:2] - 3.*dy).astype('int')
-            x4 = (centers[:,0:1] + 3.*dx).astype('int')
-            y4 = (centers[:,1:2] + 3.*dy).astype('int')
-            x3 = self._checkBounds(x3, bw.shape[1])
-            y3 = self._checkBounds(y3, bw.shape[0])
-            x4 = self._checkBounds(x4, bw.shape[1])
-            y4 = self._checkBounds(y4, bw.shape[0])
-            flag_signs = (np.logical_and(bw[y3,x3]>0, bw[y4,x4]==0)).astype('int')*2-1
-            normals = np.hstack([dx, dy]) * flag_signs
- 
-            """ # Old code with lists and loop, performs 4x slower 
-            for cnt,line in enumerate(lines):
-                x1,y1,x2,y2 = line
-                dx = 1.*(y2-y1)/((x1-x2)**2+(y1-y2)**2)**0.5
-                dy = 1.*(x1-x2)/((x1-x2)**2+(y1-y2)**2)**0.5
-                x3 = int((x1+x2)/2. - 3.*dx)
-                y3 = int((y1+y2)/2. - 3.*dy)
-                x4 = int((x1+x2)/2. + 3.*dx)
-                y4 = int((y1+y2)/2. + 3.*dy)
-                x3 = self._checkBounds(x3, bw.shape[1])
-                y3 = self._checkBounds(y3, bw.shape[0])
-                x4 = self._checkBounds(x4, bw.shape[1])
-                y4 = self._checkBounds(y4, bw.shape[0])
-                if bw[y3,x3]>0 and bw[y4,x4]==0:
-                    normals[cnt,:] = [dx, dy] 
-                else:
-                    normals[cnt,:] = [-dx, -dy]
-            """
-            self._correctPixelOrdering(lines, normals)
-        return centers, normals
+#########
+# MISE: COMPLETE THIS
+#########
+        return centers, normals, lines
 
     def detectLines(self, color):
         bw, edge_color = self._colorFilter(color)
         lines = self._HoughLine(edge_color)
-        centers, normals = self._findNormal(bw, lines)
+        centers, normals, lines = self._findNormal(bw, lines)
         return Detections(lines=lines, normals=normals, area=bw, centers=centers)
 
     def setImage(self, bgr):
